c63833ecfa47be39c8d015c2b884f998352e23f8
syrr <syrr@yahoo.com>
2025-07-13 17:54:44 +0000

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
