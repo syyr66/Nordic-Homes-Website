ac53b76f41f25651f2034e4c78be8ed9c7f8e85e
syrr <syrr@yahoo.com>
2025-07-13 19:14:53 +0000

    Change format of price field to DecimalField in Product model

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-    price = models.InteField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
