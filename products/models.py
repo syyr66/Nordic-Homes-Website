3a0607ac147fd9445349f39dcbffae458e619560
syrr <syrr@yahoo.com>
2025-07-13 18:57:13 +0000

    Created database model for products

--- products/models.py
+++ products/models.py
@@ -9,3 +9,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.InteField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.name
