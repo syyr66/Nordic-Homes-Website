e5d039e6b57fb618248d84d345d56e94304d88d2
syrr <syrr@yahoo.com>
2025-07-28 16:24:13 +0300

    Create cart template, view and urlpattern

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import add_to_cart
+from . import views
 
 app_name = "cart"
 
 urlpatterns = [
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart'),
+    # path('cart/checkout/', views.checkout, name="checkout"),
 ]
