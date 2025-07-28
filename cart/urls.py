19156e7c7f78bcb1e1a2c6a24b0910e86b80769f
syrr <syrr@yahoo.com>
2025-07-28 19:18:29 +0300

    refactor: fix cart urlpatterns

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "cart"
 
 urlpatterns = [
+    path('', views.cart, name='cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart, name='cart'),
-    path('cart/checkout/', views.checkout, name="checkout"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- nordichomes/urls.py
+++ nordichomes/urls.py
@@ -7,6 +7,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls", namespace="core")),
-    path('', include("cart.urls", namespace="cart")),
+    path('cart/', include("cart.urls", namespace="cart")),
     path('products/', include("products.urls", namespace="products")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
