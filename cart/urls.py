c0f6fb87de8d4b475c830371cfb49da383e1554c
syrr <syrr@yahoo.com>
2025-07-31 13:06:15 +0300

    enable to increase or decrease quantity of item in cart

--- cart/urls.py
+++ cart/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout, name="checkout"),
+    path('hx_menu_cart/', views.hx_menu_cart, name="hx_menu_cart"),
+    path('update_cart/<int:product_id>/<slug:action>/', views.update_cart, name="update_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .cart import Cart
+from products.models import Product
 
 
 def add_to_cart(request, product_id):
@@ -23,9 +25,43 @@ def cart(request):
         'next_page': reverse('cart:checkout'),
     })
 
+
+def update_cart(request, product_id, action):
+    cart = Cart(request)
+
+    if action == 'item_increment':
+        cart.add(product_id, update_quantity=True)
+    elif action == 'item_decrement':
+        cart.add(product_id, quantity=-1, update_quantity=True)
+
+    product = get_object_or_404(Product, id=product_id)
+
+    try:
+        quantity = cart.cart[str(product_id)]['quantity']
+    except:
+        response = HttpResponse('')
+        response['HX-Trigger'] = 'update-menu-cart'
+        return response
+
+    item = {
+        'product': product,
+        'total_price': quantity * product.price,
+        'quantity': quantity,
+    }
+
+    response = render(request, 'cart/partials/cart_item.html', {'item': item})
+    response['HX-Trigger'] = 'update-menu-cart'
+
+    return response
+
+
 @login_required()
 def checkout(request):
     return render(request, "cart/checkout.html", {
         'title' : 'Checkout',
         'next_page': reverse('core:home'), #Home page because i don't have order_detail view yet
     })
+
+
+def hx_menu_cart(request):
+    return render(request, 'cart/menu_cart.html')
