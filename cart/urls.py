610be4d01b5bb79dd7cfce406021cd4c0c0370f9
syrr <syrr@yahoo.com>
2025-08-04 13:39:45 +0300

    change order of urlpatterns in cart/urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout, name="checkout"),
+    path('update_cart/<int:product_id>/<slug:action>/', views.update_cart, name="update_cart"),
+
     path('hx_menu_cart/', views.hx_menu_cart, name="hx_menu_cart"),
     path('hx_summary/', views.hx_summary, name="hx_summary"),
-    path('update_cart/<int:product_id>/<slug:action>/', views.update_cart, name="update_cart"),
 ]
